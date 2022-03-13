44d0b602ad6935f1a651829362dda8820725220e
valeriy nezhurov <valeriy.nezhuriov@gmail.com>
2022-03-13 15:34:32 +0200

    add game screen

--- main.go
+++ main.go
@@ -1,138 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/vallerion/pingpong-go/consts"
-	"github.com/vallerion/pingpong-go/entities"
+	"github.com/vallerion/pingpong-go/screens"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/gofont/goregular"
 	"golang.org/x/image/font/opentype"
-	"image/color"
 	"log"
-	"math/rand"
 )
 
 var (
-	leftPlayer  *entities.Player
-	rightPlayer *entities.Player
-	balls       []*entities.Ball
-	border      *entities.Border
-	mainFont    font.Face
+	gameScreen *screens.GameScreen
+	mainFont   font.Face
 )
 
 func init() {
-	leftPlayer = entities.CreatePlayer(consts.GameZoneLeft+50, consts.GameZoneVerticalCenter-consts.PlayerHeight/2)
-	rightPlayer = entities.CreatePlayer(consts.GameZoneRight-(50+consts.PlayerWidth), consts.GameZoneVerticalCenter-consts.PlayerHeight/2)
-	border = entities.CreateBorder()
-
-	balls = make([]*entities.Ball, 0)
-	addBall()
-
 	fontObj, _ := opentype.Parse(goregular.TTF)
 	mainFont, _ = opentype.NewFace(fontObj, &opentype.FaceOptions{
 		Size:    32,
 		DPI:     72,
 		Hinting: font.HintingNone,
 	})
-}
 
-func addBall() {
-	balls = append(balls, entities.CreateBall())
+	gameScreen = screens.CreateScreen(mainFont)
 }
 
 type Game struct{}
 
 func (g *Game) Update() error {
-	go func() {
-		if rand.Intn(1000) == 50 {
-			addBall()
-		}
-	}()
-
-	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		leftPlayer.MoveUp()
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		leftPlayer.MoveDown()
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		rightPlayer.MoveUp()
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		rightPlayer.MoveDown()
-	}
-
-	tempBalls := make([]*entities.Ball, 0)
-	for _, ball := range balls {
-		if ball.IsDisplay == false {
-			continue
-		}
-
-		if ball.LeftGoal() {
-			ball.ResetPosition()
-			rightPlayer.Score++
-
-			if len(balls) > 1 {
-				ball.IsDisplay = false
-			}
-		}
-
-		if ball.RightGoal() {
-			ball.ResetPosition()
-			leftPlayer.Score++
-
-			if len(balls) > 1 {
-				ball.IsDisplay = false
-			}
-		}
-
-		ball.Update()
-
-		if leftPlayer.GetRect().Overlaps(*ball.GetRect()) {
-			ball.Repel(leftPlayer.GetRect())
-		}
-
-		if rightPlayer.GetRect().Overlaps(*ball.GetRect()) {
-			ball.Repel(rightPlayer.GetRect())
-		}
-
-		for _, subBall := range balls {
-			if ball == subBall {
-				continue
-			}
-
-			if subBall.GetRect().Overlaps(*ball.GetRect()) {
-				ball.Repel(subBall.GetRect())
-			}
-		}
-
-		if ball.IsDisplay {
-			tempBalls = append(tempBalls, ball)
-		}
-	}
-
-	// remove those that are not showing
-	balls = tempBalls
-
-	return nil
+	return gameScreen.Update()
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	msg := fmt.Sprintf("TPS: %0.2f FPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS())
-	ebitenutil.DebugPrint(screen, msg)
-
-	scoreText := fmt.Sprintf("%d:%d", leftPlayer.Score, rightPlayer.Score)
-	text.Draw(screen, scoreText, mainFont, consts.GameZoneHorizontalCenter-20, 40, color.White)
-
-	border.Draw(screen)
-	leftPlayer.Draw(screen)
-	rightPlayer.Draw(screen)
-	for _, ball := range balls {
-		ball.Draw(screen)
-	}
+	gameScreen.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
---
+++ screens/game.go
@@ -0,0 +1,124 @@
+package screens
+
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/vallerion/pingpong-go/consts"
+	"github.com/vallerion/pingpong-go/entities"
+	"golang.org/x/image/font"
+	"image/color"
+	"math/rand"
+)
+
+type GameScreen struct {
+	leftPlayer  *entities.Player
+	rightPlayer *entities.Player
+	balls       []*entities.Ball
+	border      *entities.Border
+	font        font.Face
+}
+
+func CreateScreen(font font.Face) *GameScreen {
+	image := &GameScreen{
+		entities.CreatePlayer(consts.GameZoneLeft+50, consts.GameZoneVerticalCenter-consts.PlayerHeight/2),
+		entities.CreatePlayer(consts.GameZoneRight-(50+consts.PlayerWidth), consts.GameZoneVerticalCenter-consts.PlayerHeight/2),
+		make([]*entities.Ball, 0),
+		entities.CreateBorder(),
+		font,
+	}
+
+	image.addBall()
+	return image
+}
+
+func (s *GameScreen) addBall() {
+	s.balls = append(s.balls, entities.CreateBall())
+}
+
+func (s *GameScreen) Update() error {
+	go func() {
+		if rand.Intn(1000) == 50 {
+			s.addBall()
+		}
+	}()
+
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		s.leftPlayer.MoveUp()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		s.leftPlayer.MoveDown()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		s.rightPlayer.MoveUp()
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		s.rightPlayer.MoveDown()
+	}
+
+	tempBalls := make([]*entities.Ball, 0)
+	for _, ball := range s.balls {
+		if ball.IsDisplay == false {
+			continue
+		}
+
+		if ball.LeftGoal() {
+			ball.ResetPosition()
+			s.rightPlayer.Score++
+
+			if len(s.balls) > 1 {
+				ball.IsDisplay = false
+			}
+		}
+
+		if ball.RightGoal() {
+			ball.ResetPosition()
+			s.leftPlayer.Score++
+
+			if len(s.balls) > 1 {
+				ball.IsDisplay = false
+			}
+		}
+
+		ball.Update()
+
+		if s.leftPlayer.GetRect().Overlaps(*ball.GetRect()) {
+			ball.Repel(s.leftPlayer.GetRect())
+		}
+
+		if s.rightPlayer.GetRect().Overlaps(*ball.GetRect()) {
+			ball.Repel(s.rightPlayer.GetRect())
+		}
+
+		for _, subBall := range s.balls {
+			if ball == subBall {
+				continue
+			}
+
+			if subBall.GetRect().Overlaps(*ball.GetRect()) {
+				ball.Repel(subBall.GetRect())
+			}
+		}
+
+		if ball.IsDisplay {
+			tempBalls = append(tempBalls, ball)
+		}
+	}
+
+	// remove those that are not showing
+	s.balls = tempBalls
+
+	return nil
+}
+
+func (s *GameScreen) Draw(image *ebiten.Image) {
+	scoreText := fmt.Sprintf("%d:%d", s.leftPlayer.Score, s.rightPlayer.Score)
+	text.Draw(image, scoreText, s.font, consts.GameZoneHorizontalCenter-20, 40, color.White)
+
+	s.border.Draw(image)
+	s.leftPlayer.Draw(image)
+	s.rightPlayer.Draw(image)
+	for _, ball := range s.balls {
+		ball.Draw(image)
+	}
+}
